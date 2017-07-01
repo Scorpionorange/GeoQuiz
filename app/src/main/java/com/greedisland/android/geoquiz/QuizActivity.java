59076e898ca84fd4c6f3cb98d3d67720b79fefe7
scorpionorange <scorpionorange@126.com>
2017-07-01 16:42:38 +0800

    updata at 2017.7.01;

--- app/src/main/java/com/greedisland/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/greedisland/android/geoquiz/QuizActivity.java
@@ -94,11 +94,19 @@ public class QuizActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(view ->
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        /*fab.setOnClickListener(view ->
             Snackbar.make(view,
                           "Replace with your own action",
                            Snackbar.LENGTH_LONG).setAction("Action", null).show()
         );
+        */
     }
 
     @Override

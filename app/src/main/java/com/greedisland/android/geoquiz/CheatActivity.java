64445263843584637662dff13b4b0256fa8f4bb5
scorpionorange <scorpionorange@126.com>
2017-08-30 23:58:49 +0800

    update at 2017.8.30;

---
+++ app/src/main/java/com/greedisland/android/geoquiz/CheatActivity.java
@@ -0,0 +1,13 @@
+package com.greedisland.android.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CheatActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
